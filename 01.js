a23a056d404fc8bc6a0bb4b98f8e051cc90d66e8
TeamDman <TeamDman9201@gmail.com>
2021-12-01 10:29:39 -0500

    Add part 2

--- 01.js
+++ 01.js
@@ -1,17 +1,50 @@
-temp1
-// <pre style=​"word-wrap:​ break-word;​ white-space:​ pre-wrap;​">​…​</pre>​
+delete print; // I keep typing this by mistake :/
 
+/* Part 1 problem formulation
 
-lines=temp1.innerText.split("\n")
-// (2001) ['140', '154', '165', '150', '151', '161', '172', '174', '166', '168', '176', '191', '192', '189', '190', '191', '202', '203', '206', '207', '167', '179', '204', '206', '208', '209', '216', '198', '200', '207', '217', '218', '223', '227', '228', '243', '241', '246', '278', '255', '256', '273', '295', '291', '288', '290', '303', '325', '301', '292', '291', '309', '313', '315', '319', '339', '340', '327', '336', '335', '340', '331', '334', '316', '317', '323', '324', '323', '312', '298', '291', '285', '288', '266', '267', '266', '258', '259', '257', '259', '250', '265', '270', '267', '271', '272', '277', '281', '277', '284', '291', '283', '284', '291', '292', '278', '285', '296', '294', '297', …]
+Given a list of integers, determine the count of values which are greater than the previous value.
 
-lines1 = lines.map(x => parseInt(x))
-// (2001) [140, 154, 165, 150, 151, 161, 172, 174, 166, 168, 176, 191, 192, 189, 190, 191, 202, 203, 206, 207, 167, 179, 204, 206, 208, 209, 216, 198, 200, 207, 217, 218, 223, 227, 228, 243, 241, 246, 278, 255, 256, 273, 295, 291, 288, 290, 303, 325, 301, 292, 291, 309, 313, 315, 319, 339, 340, 327, 336, 335, 340, 331, 334, 316, 317, 323, 324, 323, 312, 298, 291, 285, 288, 266, 267, 266, 258, 259, 257, 259, 250, 265, 270, 267, 271, 272, 277, 281, 277, 284, 291, 283, 284, 291, 292, 278, 285, 296, 294, 297, …]
+*/
 
-lines2 = lines1.map((v,i) => v > lines1?.[i-1]??v)
-// (2001) [false, true, true, false, true, true, true, true, false, true, true, true, true, false, true, true, true, true, true, true, false, true, true, true, true, true, true, false, true, true, true, true, true, true, true, true, false, true, true, false, true, true, true, false, false, true, true, true, false, false, false, true, true, true, true, true, true, false, true, false, true, false, true, false, true, true, true, false, false, false, false, false, true, false, true, false, false, true, false, true, false, true, true, false, true, true, true, true, false, true, true, false, true, true, true, false, true, true, false, true, …]
+// Get list of integers
+input = document.querySelector("pre").innerText
+	.trim()
+	.split("\n")
+	.map(x => parseInt(x));
 
-lines3 = lines2.filter(x => x)
-// (1298) [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, …]
+function countGreater(list) {
+	list = list.map((value, index) => value > list?.[index-1] ?? value)
+	console.log("greaters", list);
+	list = list.filter(value => value)
+	console.log("greaters filtered", list)
+	return list.length;
+}
 
-result = lines3.length
+part1 = countGreater(input);
+console.log(`Part 1: ${part1}`);
+
+/* Part 2 problem formulation
+
+Given a list of integers:
+- Compute a 3-width sliding window (index, index+1, index+2); current and next 2 values
+- Compute the sum of each window
+- Determine the count of sums which are greater than the previous sum
+
+*/
+
+function getWindows(list, windowSize) {
+	return list.map((value, index) => list.slice(index, index+windowSize));
+}
+
+function sum(list) {
+	return list.reduce((prev, cur) => prev+cur, 0);
+}
+
+part2 = getWindows(input, 3);
+console.log("windows", part2);
+
+part2 = part2.map(sum);
+console.log("summed windows", part2);
+
+part2 = countGreater(part2);
+console.log("Part 2: creater count", part2);
